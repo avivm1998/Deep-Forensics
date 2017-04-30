1a55bec58a51a74f36842b0b8e606d1a294e005e
avivm1998 <avivm1998@gmail.com>
2017-04-30 15:01:19 +0300

    Add files via upload
    first commit.

---
+++ kernel_module.c
@@ -0,0 +1,104 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/random.h>
+#include <asm/uaccess.h>    /* for put_user */
+#include <linux/module.h>
+#include <net/sock.h> 
+#include <linux/netlink.h>
+#include <linux/skbuff.h> 
+
+/*
+* Prototypes − this would normally go in a .h file
+*/
+
+int init_module(void);
+void cleanup_module(void);
+static void nl_recv_msg(struct sk_buff *skb);
+
+#define SUCCESS 0
+#define DEVICE_NAME "linux_project" /* Dev name as it appears in /proc/devices */
+#define BUF_LEN 80  /* Max length of the message from the device */
+#define DEBUG 1
+#define NETLINK_USER 31
+#define EMPTY_MESSAGE "NO_DATA"
+
+/*
+* Global variables are declared as static, so are global within the file.
+*/
+static char msg[BUF_LEN]; /* The msg the device will give when asked */
+static char *msg_Ptr;
+
+struct sock *nl_sk = NULL; /* The netlink socket */
+
+/*
+* This function is called when the module is loaded
+*/
+int init_module(void) {
+#ifdef DEBUG
+    printk(KERN_INFO "init module\n");
+#endif
+
+    struct netlink_kernel_cfg cfg = {
+        .input = nl_recv_msg,
+    };
+
+    nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, &cfg);
+    if (!nl_sk) {
+        printk(KERN_ALERT "Error creating socket.\n");
+        return -EFAULT;
+    }
+
+    return SUCCESS;
+}
+
+/*
+* This function is called when the module is unloaded
+*/
+void cleanup_module(void) {
+#ifdef DEBUG
+    printk(KERN_INFO "cleanup module\n");
+#endif
+
+    netlink_kernel_release(nl_sk);
+
+    printk(KERN_ALERT "Module has been removed\n");
+    printk(KERN_INFO "---------------------------------------------------\n");
+}
+
+/*
+* Methods
+*/
+
+static void nl_recv_msg(struct sk_buff *skb) {
+#ifdef DEBUG
+    printk(KERN_INFO "nl_recv_msg\n");
+#endif
+    struct sk_buff* skb_out;
+    struct nlmsghdr *nlh;
+    int msg_size = strlen(msg);
+    int pid;
+
+    memset(msg, 0, BUF_LEN);
+
+    nlh = (struct nlmsghdr *)skb->data;
+    pid = nlh->nlmsg_pid;
+
+    skb_out = nlmsg_new(msg_size, 0);
+    if(!skb_out){
+         printk(KERN_ERR "Error allocating message\n");
+         return;
+    }
+
+    nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);
+    NETLINK_CB(skb_out).dst_group = 0;
+    strncpy(nlmsg_data(nlh), msg, msg_size);
+
+    int res = nlmsg_unicast(nl_sk, skb_out, pid);
+    if(res < 0)
+        printk(KERN_ERR "Error sending the message\n");
+
+#ifdef DEBUG
+    printk(KERN_INFO "Netlink received msg payload: %s\n", (char*)nlmsg_data(nlh));
+#endif
+}
---
+++ server.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <linux/netlink.h>
+#include <unistd.h>
+
+#define PORT 1202
+#define SIZE 1024
+#define NETLINK_USER 31
+
+struct sockaddr_nl src_addr, dest_addr;
+struct nlmsghdr *nlh = NULL;
+struct iovec iov;
+int sock_fd, nl_fd;
+struct msghdr msg;
+
+struct sockaddr_in addr,client;
+char buffer[1024];
+int fd_kern, client_sock, sock_fd, c, read_bytes;
+
+int main(int argc,char** argv)
+{
+	/* Initializing the netlink socket to connect to the kernel */
+	nl_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_USER);
+	if (nl_fd < 0) {
+		perror("netlink socket");
+		return -1;
+	}
+
+	memset(&src_addr, 0, sizeof(src_addr));
+	src_addr.nl_family = AF_NETLINK;
+	src_addr.nl_pid = getpid(); /* self pid */
+
+	if(bind(nl_fd, (struct sockaddr *)&src_addr, sizeof(src_addr)) < 0) {
+		perror("netlink bind");
+		return -1;
+	}
+
+	memset(&dest_addr, 0, sizeof(dest_addr));
+	memset(&dest_addr, 0, sizeof(dest_addr));
+	dest_addr.nl_family = AF_NETLINK;
+	dest_addr.nl_pid = 0; /* For Linux Kernel */
+	dest_addr.nl_groups = 0; /* unicast */
+
+	nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(SIZE));
+	memset(nlh, 0, NLMSG_SPACE(SIZE));
+	nlh->nlmsg_len = NLMSG_SPACE(SIZE);
+	nlh->nlmsg_pid = getpid();
+	nlh->nlmsg_flags = 0;
+	/* ************************************************ */
+
+	/* Initializing the socket that connects to the app */
+	if((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("app socket");
+		return -1;
+	}
+
+    // set SO_REUSEADDR on a socket to true (1):
+    int optval = 1;
+    setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
+
+	memset(&addr,0,sizeof(struct sockaddr_in));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(PORT);
+
+	if(bind(sock_fd,(struct sockaddr *)&addr,sizeof(addr)) < 0)
+	{
+		perror("app bind");
+		return -1;
+	}
+
+	listen(sock_fd,1);
+
+	c = sizeof(struct sockaddr_in);
+
+    //accept connection from an incoming client    	
+    if((client_sock = accept(sock_fd, (struct sockaddr *)&client, (socklen_t*)&c)) < 0)
+   	{
+       	perror("app accept");
+       	return -1;
+    }
+    /* *************************************************** */
+
+	do {
+		memset(buffer, 0, SIZE);
+        read_bytes = recv(client_sock, buffer, SIZE, 0);
+        printf("%s %d\n", buffer, read_bytes);
+		strcpy(NLMSG_DATA(nlh), buffer);
+
+		iov.iov_base = (void *)nlh;
+		iov.iov_len = nlh->nlmsg_len;
+		msg.msg_name = (void *)&dest_addr;
+		msg.msg_namelen = sizeof(dest_addr);
+		msg.msg_iov = &iov;
+		msg.msg_iovlen = 1;
+
+		if(sendmsg(nl_fd, &msg, 0) < 0) {
+			perror("netlink sendmsg");
+			return -1;
+		}
+
+	} while(read_bytes > 0);
+
+	return 0;
+
+}
