eb7b858f2a41242eca717f31c83adbf28878cc45
ayalnaim <ayalnaim1359@gmail.com>
2017-05-22 18:58:59 +0300

    Update server.h

--- server.h
+++ server.h
@@ -13,6 +13,7 @@
 #define MAX_PAYLOAD 1024
 #define NETLINK_USER 31
 
+
 int main(int argc,char** argv);
 void init_netlink_socket(int* nl_fd, struct sockaddr_nl* src_addr, struct sockaddr_nl* dest_addr, struct nlmsghdr** nlh, 
 	struct nlmsghdr** nlh_in, struct iovec* iov, struct iovec* iov_in, struct msghdr* msg, struct msghdr* msg_in);
