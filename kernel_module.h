2e6bf0e29fe4b46e3cbc413f33b38baa6b92ae12
ayalnaim <ayalnaim1359@gmail.com>
2017-05-22 18:58:20 +0300

    Update kernel_module.h

--- kernel_module.h
+++ kernel_module.h
@@ -12,6 +12,13 @@
 #include <linux/netlink.h>
 #include <linux/skbuff.h> 
 
+
+#define SUCCESS 0
+#define DEBUG 1
+#define NETLINK_USER 31
+#define EMPTY_MESSAGE "NO_DATA"
+#define SIZE 1024
+
 /*
 * Prototypes
 */
@@ -19,9 +26,5 @@
 int init_module(void);
 void cleanup_module(void);
 static void nl_recv_msg(struct sk_buff *skb);
-
-#define SUCCESS 0
-#define DEBUG 1
-#define NETLINK_USER 31
-#define EMPTY_MESSAGE "NO_DATA"
-#define SIZE 1024
+int copy_data_from_memory(int start_address, int length, char* data, int buffer_length);
+void parse_input(char* input, int* start, int* length);
