f3509f106631a5e413f3e21a6a2a5dbaf4dd7072
ayalnaim <ayalnaim1359@gmail.com>
2017-05-22 16:09:01 +0300

    can only do one iteration per run

--- server.c
+++ server.c
@@ -6,20 +6,31 @@ void init_netlink_socket(int* nl_fd, struct sockaddr_nl* src_addr, struct sockad
 
 	int optval = 1;
 
+	/*memset(src_addr, 0, sizeof(*src_addr));
+	memset(dest_addr, 0, sizeof(*dest_addr));
+	memset(iov, 0, sizeof(*iov));
+	memset(msg, 0, sizeof(*msg));*/
+
 	if ((*nl_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_USER)) < 0) {
 		perror("netlink socket");
 		exit(1);
 	}
 
-	memset(&(*src_addr), 0, sizeof((*src_addr)));
-	(*src_addr).nl_family = AF_NETLINK;
-	(*src_addr).nl_pid = getpid(); // self pid 
+	//memset(src_addr, 0, sizeof(*src_addr));
+	src_addr->nl_family = AF_NETLINK;
+	src_addr->nl_pid = getpid(); /* self pid */ 
+	src_addr->nl_groups = 0; /* not in mcast group */
 
-	if(bind(*nl_fd, (struct sockaddr *)&(*src_addr), sizeof((*src_addr))) < 0) {
+	if(bind(*nl_fd, (struct sockaddr *)src_addr, sizeof(*src_addr)) < 0) {
 		perror("netlink bind");
 		exit(1);
 	}
 
+	//memset(dest_addr, 0, sizeof(*dest_addr));
+	dest_addr->nl_family = AF_NETLINK;
+	dest_addr->nl_pid = 0; /* Linux kernel */
+	dest_addr->nl_groups = 0; /* unicast */
+	
     setsockopt(*nl_fd, SOL_SOCKET, NETLINK_NO_ENOBUFS, &optval, sizeof(optval));
 
 	*nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
@@ -28,20 +39,23 @@ void init_netlink_socket(int* nl_fd, struct sockaddr_nl* src_addr, struct sockad
     (*nlh)->nlmsg_pid = getpid();
     (*nlh)->nlmsg_flags = 0;
 
-    (*iov).iov_base = (void *)(*nlh);
-    (*iov).iov_len = (*nlh)->nlmsg_len;
-    (*msg).msg_name = (void *)&(*dest_addr);
-    (*msg).msg_namelen = sizeof((*dest_addr));
-    (*msg).msg_iov = &(*iov);
-    (*msg).msg_iovlen = 1;
+    /*iov->iov_base = (void *)(*nlh);
+    iov->iov_len = (*nlh)->nlmsg_len;
+    msg->msg_name = (void *)dest_addr;
+    msg->msg_namelen = sizeof(*dest_addr);
+    msg->msg_iov = iov;
+    msg->msg_iovlen = 1;*/
                                             
-    *nlh_in = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+    /**nlh_in = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
     memset(*nlh_in, 0, NLMSG_SPACE(MAX_PAYLOAD));
+    (*nlh_in)->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
+    (*nlh_in)->nlmsg_pid = getpid();
+    (*nlh_in)->nlmsg_flags = 0;
 
-    (*iov_in).iov_base = (void *)(*nlh_in);
-    (*iov_in).iov_len = (*nlh)->nlmsg_len;
-    (*msg_in).msg_iov = &(*iov_in);
-    (*msg_in).msg_iovlen = 1;
+    iov_in->iov_base = (void *)(*nlh_in);
+    iov_in->iov_len = (*nlh_in)->nlmsg_len;
+    msg_in->msg_iov = iov_in;
+    msg_in->msg_iovlen = 1;*/
 }
 
 /* Initializing the socket that connects to the client */
@@ -57,11 +71,11 @@ void init_tcp_socket(int* sock_fd, int* client_fd, struct sockaddr_in* addr, str
     setsockopt(*sock_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
 	memset(&(*addr), 0, sizeof(struct sockaddr_in));
-	(*addr).sin_family = AF_INET;
-	(*addr).sin_addr.s_addr = INADDR_ANY;
-    (*addr).sin_port = htons(PORT);
+	addr->sin_family = AF_INET;
+	addr->sin_addr.s_addr = INADDR_ANY;
+    addr->sin_port = htons(PORT);
 
-	if(bind(*sock_fd, (struct sockaddr *)&(*addr), sizeof((*addr))) < 0) {
+	if(bind(*sock_fd, (struct sockaddr *)addr, sizeof((*addr))) < 0) {
 		perror("app bind");
 		exit(1);
 	}
@@ -69,7 +83,7 @@ void init_tcp_socket(int* sock_fd, int* client_fd, struct sockaddr_in* addr, str
 	listen(*sock_fd, 1);
 
 	size = sizeof(struct sockaddr_in);    	
-    if((*client_fd = accept(*sock_fd, (struct sockaddr *) &(*client), (socklen_t*) &size)) < 0) {
+    if((*client_fd = accept(*sock_fd, (struct sockaddr *) client, (socklen_t*) &size)) < 0) {
        	perror("app accept");
        	exit(1);
     }
@@ -77,48 +91,62 @@ void init_tcp_socket(int* sock_fd, int* client_fd, struct sockaddr_in* addr, str
 
 int main(int argc,char** argv)
 {
-	int sock_fd;
-	int client_fd;
-	struct sockaddr_in addr; 
-	struct sockaddr_in client;
-	int size;
-	int read_bytes;
-	char buffer[MAX_PAYLOAD];
+	int sock_fd = 0;
+	int client_fd = 0;
+	struct sockaddr_in addr = { 0 }; 
+	struct sockaddr_in client = { 0 };
+	int size = 0;
+	int read_bytes = 0;
+	char buffer[MAX_PAYLOAD] = { 0 };
 
-	int nl_fd;
-	struct sockaddr_nl src_addr;
-	struct sockaddr_nl dest_addr;
+	int res;
+
+	int nl_fd = 0;
+	struct sockaddr_nl src_addr = { 0 };
+	struct sockaddr_nl dest_addr = { 0 };
 	struct nlmsghdr *nlh = NULL;
 	struct nlmsghdr *nlh_in = NULL;
-	struct iovec iov;
-	struct iovec iov_in;
-	struct msghdr msg;
-	struct msghdr msg_in;
+	struct iovec iov = { 0 };
+	struct iovec iov_in = { 0 };
+	struct msghdr msg = { 0 };
+	struct msghdr msg_in = { 0 };
 	
 	init_netlink_socket(&nl_fd, &src_addr, &dest_addr, &nlh, &nlh_in, &iov, &iov_in, &msg, &msg_in);
 	init_tcp_socket(&sock_fd, &client_fd, &addr, &client);
 
-	do {
-		memset(buffer, 0, MAX_PAYLOAD);
-        read_bytes = recv(client_fd, buffer, MAX_PAYLOAD, 0);
-		strcpy(NLMSG_DATA(nlh), buffer);
-		printf("%s\n", (char*)NLMSG_DATA(nlh));
+	//memset(buffer, 0, MAX_PAYLOAD);
+    read_bytes = recv(client_fd, buffer, MAX_PAYLOAD, 0);
 
-		printf("Sending message to kernel\n");
-		if(sendmsg(nl_fd, &msg, 0) < 0) {
-			perror("sendmsg");
-			return -1;
-		}
-		printf("Waiting for message from kernel\n");
+	strncpy(NLMSG_DATA(nlh), buffer, strlen(buffer) + 1);
+	printf("%s\n", (char*)NLMSG_DATA(nlh));
 
-		/* Read message from kernel */
-		if(recvmsg(nl_fd, &msg_in, 0) < 0) {
-			perror("recvmsg");
-			return -1;
-		}
-		printf("Received message payload: %s\n", (char*)NLMSG_DATA(nlh_in));
+	//memset(&iov, 0, sizeof(iov));
+	//memset(&msg, 0, sizeof(msg));
 
-	} while(read_bytes > 0);
+	iov.iov_base = (void *)nlh;
+	iov.iov_len = nlh->nlmsg_len;
+	msg.msg_name = (void *)&dest_addr;
+	msg.msg_namelen = sizeof(dest_addr);
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+
+	printf("Sending message to kernel\n");
+	res = sendmsg(nl_fd,&msg, 0);
+	if(res < 0) {
+		perror("sendmsg");
+		return -1;
+	}
+	printf("Waiting for message from kernel\n");
+
+	/* Read message from kernel */
+	res = recvmsg(nl_fd, &msg, 0);
+	printf("%d\n",res);
+	if(res < 0) {
+		perror("recvmsg");
+		return -1;
+	}
+	printf("Received message payload: %s\n", (char*)NLMSG_DATA(nlh));
+	close(nl_fd);
 
 	return 0;
 
