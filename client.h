9b9859a4d7dcd81aea12a3abc21602ccffbcdd83
avivm1998 <avivm1998@gmail.com>
2017-06-18 13:30:10 +0300

    Added new defines.

--- client.h
+++ client.h
@@ -7,10 +7,14 @@
 #include <netinet/in.h> 
 #include <sys/socket.h> 
 #include <unistd.h>
+#include "utilities.h"
 
 #define PORT 1202    /* the port client will be connecting to */
 #define MAXDATASIZE 100 /* max number of bytes we can get at once */
 #define IP "127.0.0.1"
+#define INVALID_INPUT "invalid input"
+#define EXIT "exit"
+
 
 int main();
 void init_socket(int* sockfd, struct hostent** he, struct sockaddr_in* their_addr);
